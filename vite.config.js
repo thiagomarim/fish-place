0e24c4a907d577860659b296f887b52f9a4aaec7
Thiago <thiago.marim2005@gmail.com>
2024-01-04 21:48:13 -0300

    configuracoes de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite/fish-place",
+  base: "/fish-place",
   plugins: [react(), svgr()],
 });
